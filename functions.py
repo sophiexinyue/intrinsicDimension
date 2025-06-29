1b64549331ae61fa782f99e1e510365742bdedcd
sophiexinyue <sophiepan2004@gmail.com>
2025-06-29 16:35:27 -0600

    test case for doubling dim and random sampling for correlation dim. I keep getting 0 for everything idk

--- functions.py
+++ functions.py
@@ -2,7 +2,7 @@ import math
 import random 
 from tqdm import tqdm
 
-def correlation_dim(n, r, S_n):
+def correlation_dim(n, r, S_n, sample_size):
     """
     Implementation of equation (1) from Levine and Bickel (2001) 
     for estimating the correlation dimension of a set of points in a metric space.
@@ -21,22 +21,16 @@ def correlation_dim(n, r, S_n):
     float
     correlation dimension
     """
-    # Validate inputs
-    if n <=0 or r <= 0 or not S_n:
-        raise ValueError("n must be positive, r must be positive, and S_n must not be empty.")
-    if len(S_n) != n:
-        raise ValueError("Length of S_n must match n.")
-    
     
     # Calculate the correlation dimension
     count = 0
-    for i in tqdm(range(n)): #sum from i=1 to n
-        for j in range(i + 1, n): #sum from j=i+1 to n
-            # Calculate the Euclidean distance between points S_n[i] and S_n[j]
-            distance = sum((S_n[i][k] - S_n[j][k]) ** 2 for k in range(len(S_n[0]))) ** 0.5
-            if distance < r:
-                count += 1   
-    C_n_r = count * (2 / (n * (n-1)))
+    for p in tqdm(range(sample_size)):
+        i, j = random.sample(range(n), 2)
+        distance = sum((S_n[i][k] - S_n[j][k]) ** 2 for k in range(len(S_n[0]))) ** 0.5
+        if distance < r:
+            count += 1   
+    C_n_r = count / sample_size
+    print("C_n_r:", C_n_r)
 
     # return the correlation dimension
     if C_n_r > 0:
@@ -102,9 +96,9 @@ def doubling_dim(X, r, num_centers):
     """
     ### create an approximator for doubling dimension
     ### link: https://en.wikipedia.org/wiki/Doubling_space
-
+    X = [tuple(p) for p in X]
     M_vals = []
-    for i in range(num_centers):
+    for i in tqdm(range(num_centers)):
         x = random.choice(X)
         ball = [y for y in X if euclidean(x, y) < r]
         M = count_covers(ball, r / 2)
--- tests.py
+++ tests.py
@@ -5,4 +5,5 @@ from IPython import embed
 # Load the PBMC3K dataset (Peripheral Blood Mononuclear Cells, ~3k cells)
 adata = sc.datasets.pbmc3k().X.toarray()
 
-correlation_dim(len(adata), 5, adata.tolist())
+print(correlation_dim(len(adata), 5, adata.tolist(), 2000))
+print(doubling_dim(adata.tolist(), 5, 10))
