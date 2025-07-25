ab36082b15bdcffb8fe9e4eafe68fa3cefadd339
sophiexinyue <sophiepan2004@gmail.com>
2025-07-24 21:34:09 -0600

    PCA estimator in/usr/local/bin/python3 /Users/sophiepan/Desktop/Research/intrinsic_dim/intrinsicDimension/tests.py

--- tests.py
+++ tests.py
@@ -2,6 +2,11 @@ from functions import *
 import scanpy as sc
 import numpy as np
 from IPython import embed
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
+import numpy as np
+
+pca = PCA()
 
 """# Load the PBMC3K dataset (Peripheral Blood Mononuclear Cells, ~3k cells)
 adata = sc.datasets.pbmc3k().X.toarray()
@@ -31,7 +36,17 @@ def synthetic_subspace(D,d,n,signal_scale=1,noise_scale=0):
     return X
 
 # synthetic datasets
-#synth_data = synthetic_subspace(D=20,d=3,n=1000)
+synth_data = synthetic_subspace(D=20,d=3,n=1000)
+pca.fit(synth_data)
+
+plt.figure(figsize=(8, 4))
+plt.plot(np.arange(1, len(pca.explained_variance_)+1), pca.explained_variance_, 'o-')
+plt.xlabel('Principal component')
+plt.ylabel('Eigenvalue (explained variance)')
+plt.title('PCA Spectrum (Scree Plot)')
+plt.grid(True)
+plt.show()
+plt.savefig("pca_scree_plot.png")
 
 #print(correlation_dim(synth_data.tolist(), num_trials = 10))
 #print(doubling_dim(synth_data.tolist(), num_trials = 10))
